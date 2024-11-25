cf941ec1532ddfccbe6205e6f8bd95e31befe663
Aidan Guenther <aidancguenther@gmail.com>
2024-11-24 20:04:58 -0500

    Prepared import_audio function to cooperate with UI, converted .wav files to type int16

--- model_wave.py
+++ model_wave.py
@@ -2,10 +2,14 @@
 from scipy.io import wavfile
 
 def import_audio():
-    file_path = input("Enter the file name of an audio file: ")
+    # This variable will be used by the UI to display the name of the file.
+    # The input function will be removed and replaced by however it can be
+    # inputted through the UI.
+    file_name = input("Enter the file name of an audio file: ")
     try:
         # Debugging statements
-        sample_rate, audio_data = wavfile.read(file_path)
+        sample_rate, audio_data = wavfile.read(file_name)
+        print(f"File name: {file_name}")
         print(f"Sample Rate: {sample_rate} Hz")
         print(f"Audio Data Type: {audio_data.dtype}")
         print(f"Audio Data Shape: {audio_data.shape}")
