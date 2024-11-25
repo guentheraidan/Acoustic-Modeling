b80ed0b6b16b61385a1693a11c0c6926c4de4074
Aidan Guenther <aidancguenther@gmail.com>
2024-11-24 19:46:02 -0500

    Added function to import a .wav file

--- model_wave.py
+++ model_wave.py
@@ -1,1 +1,16 @@
-#Test
+# pip install scipy
+from scipy.io import wavfile
+
+def import_audio():
+    file_path = input("Enter the file name of an audio file: ")
+    sample_rate, audio_data = wavfile.read(file_path)
+
+    # Debugging statements
+    print(f"Sample Rate: {sample_rate} Hz")
+    print(f"Audio Data Type: {audio_data.dtype}")
+    print(f"Audio Data Shape: {audio_data.shape}")
+
+def main():
+    import_audio()
+
+main()
