e1ee52f35f974343cf874aa832e5ef7a3b93c17c
Aidan Guenther <aidancguenther@gmail.com>
2024-11-24 19:55:40 -0500

    Changed import_audio function

--- model_wave.py
+++ model_wave.py
@@ -3,12 +3,14 @@ from scipy.io import wavfile
 
 def import_audio():
     file_path = input("Enter the file name of an audio file: ")
-    sample_rate, audio_data = wavfile.read(file_path)
-
-    # Debugging statements
-    print(f"Sample Rate: {sample_rate} Hz")
-    print(f"Audio Data Type: {audio_data.dtype}")
-    print(f"Audio Data Shape: {audio_data.shape}")
+    try:
+        # Debugging statements
+        sample_rate, audio_data = wavfile.read(file_path)
+        print(f"Sample Rate: {sample_rate} Hz")
+        print(f"Audio Data Type: {audio_data.dtype}")
+        print(f"Audio Data Shape: {audio_data.shape}")
+    except ValueError as e:
+        print(f"Error reading .wav file: {e}")
 
 def main():
     import_audio()
