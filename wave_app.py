c480d9f82db0a574b8c0f15562eb23e18001b973
Le Vi Pham <levipham05@gmail.com>
2024-11-27 17:35:28 -0500

    Added wave_app.py

---
+++ wave_app.py
@@ -0,0 +1,22 @@
+#import model here
+#import model here
+from interface import Interface
+
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #window size - adjustable. Set default size and position: 800x600, 300 pixels from the left and 150 pixels from the top
+        self.geometry("800x600+300+150")
+
+        self.title('Acoustic Modeling App')
+
+        #interface
+        interface = Interface(self)
+        interface.grid(row=0, column=0,  padx=10, pady=10)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
