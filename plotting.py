1e52a7d3564848c9d8fc14acdf51a69ecff613d6
Aidan Guenther <aidancguenther@gmail.com>
2024-11-27 19:11:01 -0500

    Updated waveform and frequency functionality

--- plotting.py
+++ plotting.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 
 wav_fname = 'Clap_AulaMagna_1.wav'
 sample_rate, data = wavfile.read(wav_fname)
-spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+
+if len(data.shape) > 1: # if multi-channel
+    print(f"Converted {data.shape[1]} channels to mono")
+    data = np.mean(data, axis=1) # average channels
 
 print(f"number of channels = {data.shape[len(data.shape) -1]}")
 print(f"sample rate = {sample_rate}Hz")
@@ -13,15 +16,19 @@ length = data.shape[0] / sample_rate
 print(f"length = {length}s")
 
 time = np.linspace(0., length, data.shape[0])
-plt.plot(time, data[:, 0], label="Left channel")
-plt.plot(time, data[:, 1], label = "Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
+
+plt.figure()
+plt.plot(time, data)
+plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
+plt.title("Waveform Graph")
 plt.show()
 
+plt.figure()
+spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
 cbar = plt.colorbar(im)
+cbar.set_label('Intensity (dB)')
 plt.xlabel('Time (s)')
 plt.ylabel('Frequency (Hz)')
-cbar.set_label('Intensity (dB)')
+plt.title("Frequency Graph")
 plt.show()
