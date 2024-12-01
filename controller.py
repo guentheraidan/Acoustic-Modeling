6f0796448b72c9f38611b2f4572e1f692b768b64
Aidan Guenther <aidancguenther@gmail.com>
2024-12-01 17:41:37 -0500

    Removed unnecessary variables in Controller, added more comments to Controller and Model

--- controller.py
+++ controller.py
@@ -4,58 +4,32 @@ class Controller:
         self.view = view
 
     def analyze_file(self, file_name):
-        # this fills the data in the model object
+        # if the file is a .mp3 file, convert to .wav
         if file_name[-4::] == '.mp3':
             self.model.mp3_to_wav(file_name)
+        # if the file is a .wav file, send as is
         elif file_name[-4::] == '.wav':
             self.model.set_file_name(file_name)
 
-        self.model.channel_handler()
-        self.model.compute_frequency()
+        self.model.channel_handler() # convert multiple channels to mono
+        self.model.compute_frequency() # get spectrogram data
 
-        # can send these variables to create graphs in View
-        # and display info about the graph
-        self.view.time = self.model.get_time()
-        self.view.data = self.model.get_data()
-        self.view.length = self.model.get_length()
+        # access values to display data and create waveform and frequency graphs
+        self.view.time = self.model.time
+        self.view.data = self.model.data
+        self.view.length = self.model.length
         self.view.rfrequency = self.model.get_highest_resonance()
+        self.view.sample_rate = self.model.sample_rate
 
-        self.view.sample_rate = self.model.get_sample_rate()
-        # use this line in View (you wont need the first 3 variables):
-        # spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
-
-        # Below is for RT60 graphs. These are displayed with:
-        #plt.figure(2)
-        #plt.plot(t, data_in_db, linewidth=1, alpha=0.7, color='#004bc6')
-        #plt.xlabel('Time (s)')
-        #plt.ylabel('Power (dB)')
-        #plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
-        #plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
-        #plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
-        #plt.xlim(0, ((round(abs(rt60), 2)) * 1.5))
-        #plt.grid()
-        #plt.show()
-
+        # access time array for RT60 graphs
         self.view.t = self.model.t
 
-        # data_in_db_### is used to create the initial plot
-        # points_### hold three arrays, each with two coordinates
-        #    the first array is max, second is max_less_5, and third is max_less_25
-        #       the first coordinate in first array replaces t[index_of_max]
-        #       the second coordinate in first array replaces data_in_db[index_of_max]
-
-        #       the first coordinate in second array replaces t[index_of_max_less_5]
-        #       the second coordinate in second array replaces data_in_db[index_of_max_less_5]
-
-        #       the first coordinate in third array replaces t[index_of_max_less_25]
-        #       the second coordinate in third array replaces data_in_db[index_of_max_less_25]
-
+        # access values for low, mid, and high RT60 graphs
+        #   points_[low/mid/high] will hold three points: [0] = max, [1] = max less 5, [2] = max less 25
+        #     each point will have two values: [0] = time (s), [1] = value (dB)
         self.view.data_in_db_low, self.view.points_low, rt60_low = self.model.compute_rt60(250)
         self.view.data_in_db_mid, self.view.points_mid, rt60_mid = self.model.compute_rt60(1000)
         self.view.data_in_db_high, self.view.points_high, rt60_high = self.model.compute_rt60(5000)
-        
-        self.view.rt60_low = rt60_low
-        self.view.rt60_mid = rt60_mid
-        self.view.rt60_high = rt60_high
 
+        # average RT60 values and compute less 0.5
         self.view.difference = self.model.compute_difference(rt60_low, rt60_mid, rt60_high)
